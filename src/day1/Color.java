31929cb0002a7b946fca0ea4cb6aa8eef2f3ba7d
Semra Bahadir <sb115@students.uwf.edu>
2020-03-11 20:08:56 -0400

    Added colors class

---
+++ src/day1/Color.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class Color {
+    public static void main(String[] args) {
+        String [] color = {"red", "blue" ,"green"};
+        System.out.println(color);
+    }
+}
